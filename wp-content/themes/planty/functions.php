2c028cb40b9bc9b42425724e9b486acaf0010ad0
Matheo TRIKI <matheo.triki@gmail.com>
2024-05-20 18:47:27 +0200

    Début modification responsive mobile

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -41,23 +41,6 @@ add_filter('wp_nav_menu_items', 'filter_admin', 10, 2);
 
 
 
-function filter_admin_menu_item($items, $args) {
-    if ($args->theme_location === 'primary') {
-        // Création du lien Admin avec un href vers le tableau de bord WordPress
-        $admin_link = '<li class="menu-item"><a href="#">Admin</a></li>';
-        // Position où vous voulez que le lien Admin apparaisse
-        $insert_after = 'Nous rencontrer</li>';
-        
-        // Si l'utilisateur est connecté et est un administrateur
-        if (is_user_logged_in() && current_user_can('manage_options')) {
-            // Insérer le lien Admin après "Nous rencontrer"
-            $items = str_replace($insert_after, $insert_after . $admin_link, $items);
-        }
-    }
-    return $items;
-}
-add_filter('wp_nav_menu_items', 'filter_admin_menu_item', 10, 2);
-
 
 
 
